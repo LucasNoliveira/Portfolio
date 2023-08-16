ac92c810719e6264451ef56961345037501bfa89
Lucas Oliveira <lucasnoliveira126@gmail.com>
2023-08-16 14:42:51 -0300

    topnav modo escuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,37 @@
-function changeMode() {
-    var body = document.body;
+    function changeMode() {
+        const body = document.body;
+        const nav = document.querySelector('.nav');
+        const button = document.getElementById('button');
+        const randomImg = document.querySelector('.random-img');
+        const links = nav.querySelectorAll('.nav-items'); // Selecionar apenas os links dentro de .nav-items
 
-    body.classList.toggle("dark-theme");
-    let button = document.getElementById('button');
+        body.classList.toggle("dark-theme");
+        nav.classList.toggle('dark-theme');
+        randomImg.classList.toggle('dark-theme-image');
 
-    if (button.innerHTML == "Dark Mode") {
-        button.innerHTML = "Normal Mode";
-    } else {
-        button.innerHTML = "Dark Mode"
+        if (button.innerHTML === "Dark Mode") {
+            button.innerHTML = "Normal Mode";
+        } else {
+            button.innerHTML = "Dark Mode";
+        }
+
+        for (const link of links) {
+            if (nav.classList.contains('dark-theme')) {
+                link.style.color = 'white';
+            } else {
+                link.style.color = 'black'; // Cor branca padrão no modo claro
+            }
+
+            link.addEventListener('mouseover', function() {
+                this.style.color = 'tomato';
+            });
+
+            link.addEventListener('mouseout', function() {
+                if (nav.classList.contains('dark-theme')) {
+                    this.style.color = 'white';
+                } else {
+                    link.style.color = 'black'; // Cor branca padrão no modo claro
+                }
+            });
+        }
     }
-}
-
-function toggleTheme() {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('dark-theme');
-}
-
-function titleTheme() {
-    const title = document.querySelector('.title');
-    title.classList.toggle('dark-theme');
-}
