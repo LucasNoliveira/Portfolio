54e62c451629abe01429467f445f03a5045208b0
Lucas Oliveira <lucasnoliveira126@gmail.com>
2023-09-22 17:59:31 -0300

    dark mode animation button (still with bugs)

--- js/main.js
+++ js/main.js
@@ -5,13 +5,18 @@ const whiteText = whiteContainer.querySelector('p')
 
 let isLight = true;
 
-function lightMode (){
+function lightMode() {
     whiteBody.classList.toggle('light-mode')
-    whiteText.classList.toggle ('light-mode')
+    whiteText.classList.toggle('light-mode')
     buttonText.innerHTML.toggle = 'Dark'
 
     isLight = !isLight;
 
     buttonText.textContent = isLight ? 'Dark' : 'Light';
 }
-whiteBody.addEventListener('click' , lightMode)
+whiteBody.addEventListener('click', lightMode)
+
+$('.toggle').click(function () {
+    $(".moon").toggleClass('sun');
+    $(".toggle").toggleClass('day');
+});
