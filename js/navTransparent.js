1df3b31d03872a7eabe090525d4d34cccc3aac89
Lucas Oliveira <lucasnoliveira126@gmail.com>
2023-10-06 20:29:45 -0300

    nav muda de cor com scroll código criado

---
+++ js/navTransparent.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 100) {
+//             $("header").css("background", "white");
+//         }
+
+//         else {
+//             $("header").css("background", "transparent");
+//         }
+//     })
+// })
